210607c9fe4893bc11a75403a3109cdaf70c7031
Ubuntu <ubuntu@ip-172-31-42-134.ec2.internal>
2018-10-04 16:36:58 +0000

    fixed type with path

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    paht('home/', include('home.urls')),
+    path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
